5f0731c3ee0e7fa3a3a31782a962bb1a264a86eb
league <league@imac-34.attlocal.net>
2021-09-09 18:30:39 -0700

    Jeopardy

--- src/_03_jeopardy/Jeopardy.java
+++ src/_03_jeopardy/Jeopardy.java
@@ -66,16 +66,16 @@ public class Jeopardy implements ActionListener {
 		quizPanel.add(firstButton);
 		// 8. Write the code to complete the createButton() method below. Check that your
 		// game looks like Figure 1 in the Jeopardy Handout - http://bit.ly/1bvnvd4.
-
+		
 		// 9. Use the secondButton variable to hold a button using the createButton
-		// method
-
+		secondButton = createButton("20");
 		// 10. Add the secondButton to the quizPanel
-
+		quizPanel.add(secondButton);
 		// 11. Add action listeners to the buttons (2 lines of code)
-
+		firstButton.addActionListener(this);
+		secondButton.addActionListener(this);
 		// 12. Write the code to complete the actionPerformed() method below
-
+		
 		// 13. Add buttons so that you have $200, $400, $600, $800 and $1000 questions
 		
 		 /*
@@ -101,7 +101,7 @@ public class Jeopardy implements ActionListener {
 		buttonCount+=1;
 		// Return your new button instead of the temporary button
 		
-		return new JButton("temporary button");
+		return button;
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -111,13 +111,17 @@ public class Jeopardy implements ActionListener {
 
 		JButton buttonPressed = (JButton) e.getSource();
 		// If the buttonPressed was the firstButton
-
+		if (buttonPressed==firstButton) {
+			askQuestion("Why is a lightbulb yellow?", "Because it glows.", 10);
+		}
 			// Call the askQuestion() method
- 
+		
 		// Complete the code in the askQuestion() method. When you play the game, the score should change.
 
 		// If the buttonPressed was the secondButton
-
+		if (buttonPressed==secondButton) {
+			askQuestion("Why is the ocean blue?", "Because the blue paint from boats stained the water.", 20);
+		}
 			// Call the askQuestion() method with a harder question
 
 		// Clear the text on the button that was pressed (set the button text to nothing)
@@ -127,25 +131,32 @@ public class Jeopardy implements ActionListener {
 	private void askQuestion(String question, String correctAnswer, int prizeMoney) {
 		
 		// Use the playJeopardyTheme() method to play music while the use thinks of an answer
-		
+		playJeopardyTheme();
 		// Remove this temporary message and replace it with a pop-up that asks the user the question
-		JOptionPane.showMessageDialog(null, "this is where the question will be asked");
+		String answer = JOptionPane.showInputDialog(null, question);
 		
 		// Stop the theme music when they have entered their response. 
-		
+		stopJeopardyTheme();
 		// If the answer is correct
-
+		if (answer.equals(correctAnswer)) {
+			score+=prizeMoney;
+			JOptionPane.showMessageDialog(null, "You are correct.");
+		}
 			// Increase the score by the prizeMoney
-
+		
 			// Pop up a message to tell the user they were correct
 
 		// Otherwise
-
+		else {
+			score-=prizeMoney;
+			JOptionPane.showMessageDialog(null, "You are incorrect.");
+		}
 			// Decrement the score by the prizeMoney
 
 			// Pop up a message to tell the user they were wrong and give them the correct answer
 
 		// Call the updateScore() method
+		updateScore();
 
 	}
 
