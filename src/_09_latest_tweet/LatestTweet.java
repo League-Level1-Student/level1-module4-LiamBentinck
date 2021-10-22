3303f0be4e78a64014e160610d555c76b7f34420
league <league@imac-16.attlocal.net>
2021-10-21 18:26:41 -0700

    Finished Lastest Tweet and also piglatintranslator

--- src/_09_latest_tweet/LatestTweet.java
+++ src/_09_latest_tweet/LatestTweet.java
@@ -5,17 +5,26 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
 
 public class LatestTweet implements ActionListener {
 
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JTextField text = new JTextField(8);
+	JLabel label = new JLabel();
 	JButton button = new JButton("Search the Twitterverse");
 	LatestTweet(){
 		button.addActionListener(this);
+		panel.add(label);
 		panel.add(text);
 		panel.add(button);
 		frame.add(panel);
@@ -26,21 +35,34 @@ public class LatestTweet implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if (e.getSource()==button) {
-			System.out.println("Tweet, Tweet");
+			String search = text.getText();
+			String tweet = getLatestTweet(search);
+			System.out.println(tweet);
+			label.setText(tweet);
 		}
+		
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	private String getLatestTweet(String searchingFor) {
+
+	    Twitter twitter = new TwitterFactory().getInstance();
+
+	    AccessToken accessToken = new AccessToken(
+	        "2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+	        "vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+	    twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN",
+	        "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+	    twitter.setOAuthAccessToken(accessToken);
+
+	Query query = new Query(searchingFor);
+	    try {
+	        QueryResult result = twitter.search(query);
+	        return result.getTweets().get(0).getText();
+	    } catch (Exception e) {
+	        System.err.print(e.getMessage());
+	        return "What the heck is that?";
+	    }
+	}
 	
 	
 	
